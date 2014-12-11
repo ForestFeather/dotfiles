bad50615c11916aafd2b19f0803dbbf00a0f4588
Collin O'Connor <collin.d.oconnor@gmail.com>
2014-12-11 08:56:50 -0600

    Working on mutt a bit more; fixing the mutt temp directory issue and getting some more settings in place.

--- setup.sh
+++ setup.sh
@@ -43,8 +43,7 @@ unlink_file() {
 }
 
 # Set up directories
-p
-foldersArr=(scripts bin .ssh .mutt .mail .offlineimap .screen .vim .config '.config/fontconfig' '.config/fontconfig/conf.d'  '.vim/colors' '.vim/bundle' '.vim/plugin' .tmux .git .fonts Applications Documents Downloads DUMP Movies Music Pictures Projects Sites Landfill)
+foldersArr=(scripts bin .ssh .mutt '.mutt/temp' .mail .offlineimap .screen .vim .config '.config/fontconfig' '.config/fontconfig/conf.d'  '.vim/colors' '.vim/bundle' '.vim/plugin' .tmux .git .fonts Applications Documents Downloads DUMP Movies Music Pictures Projects Sites Landfill)
 echo "Creating directories."
 for LINK in ${foldersArr[@]}; do
 	if [ ! -d "$HOME/$LINK" ]; then
