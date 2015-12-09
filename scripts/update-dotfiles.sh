6bf0ce12a866724a41b57faa868ace9e52c8b10c
Collin O'Connor <collin.d.oconnor@gmail.com>
2015-12-09 08:55:30 -0600

    Adding to file update system.

---
+++ scripts/update-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Updates dotfiles.  Intended to be run once per week on established systems.
+# Author: Collin 'Ridayah' O'Connor <ridayah@gmail.com>
+#############################################################################
+
+cd ~/.dotfiles
+git pull --all --verbose
